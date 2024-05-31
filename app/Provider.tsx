38a340f50b99c65bfe307a14f716ac9fc1489a78
AntisocialDev <victorolaolubanjo@yahoo.com>
2024-05-30 23:36:55 -0400

    Slow fix

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,12 +2,18 @@
 
 import { useEffect, useState } from 'react';
 import AppFooter from "@/components/AppFooter";
+import bg1 from '../public/images/bg1.png'
+import bg2 from '../public/images/bg2.png'
+import bg3 from '../public/images/bg3.png'
+import bg4 from '../public/images/bg4.png'
+import { StaticImageData } from 'next/image';
 
-const bgImages: string[] = [
-    '/images/bg1.png',
-    '/images/bg2.png',
-    '/images/bg3.png',
-    '/images/bg4.png',
+
+const bgImages: StaticImageData[] = [
+    bg1,
+    bg2,
+    bg3,
+    bg4
 ];
 
 export default function Provider({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -17,7 +23,7 @@ export default function Provider({ children }: Readonly<{ children: React.ReactN
         const interval = setInterval(() => {
             setCurrentBgIndex((prevIndex) => (prevIndex + 1) % bgImages.length);
         }, 4000);
-        document.body.style.setProperty('--background-image-url', `url(${bgImages[currentBgIndex]})`);
+        document.body.style.setProperty('--background-image-url', `url(${bgImages[currentBgIndex].src})`);
         return () => clearInterval(interval);
     }, [currentBgIndex]);
 
